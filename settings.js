1e7e8476d08499198387d023d5b5e03d6e769242
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-10-17 21:07:51 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ const settings = {
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.5.1",
-  updateZipUrl: "https://github.com/vinpink2/june-private-repohide/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/vinpink2/Serbage-X/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
